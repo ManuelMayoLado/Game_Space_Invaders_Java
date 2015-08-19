906d443675348e8fef24a30647044932ad4e985d
manuel <manuel.mayo.lado@gmail.com>
2015-08-19 06:28:24 +0200

    update

--- src/main.java
+++ src/main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 
+import java.util.Random;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.Color;
@@ -8,7 +10,7 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
-import org.newdawn.slick.Font;
+import org.newdawn.slick.Image;
 
 
 public class main extends BasicGame {
@@ -29,7 +31,7 @@ public class main extends BasicGame {
 	
 	private static float velocidadePj = 2.5f;
 	
-	private static int cargaBala = 40;
+	private static int cargaBala = 50;
 	
 	private static int anchoCadro = 10;
 	private static int altoCadro = 10;
@@ -46,8 +48,13 @@ public class main extends BasicGame {
 	private float descansoMarcianos0 = descansoMarcianosInicio;
 	private float descansoMarcianos = descansoMarcianos0;
 	
-	private static float anchoMarciano = 25;
-	private static float altoMarciano = 25;
+	private static int anchoMarciano = 25;
+	private static int altoMarciano = 25;
+	
+	private static int anchoBalaMarciano = 5;
+	private static int altoBalaMarciano = 5;
+	
+	private static float velocidadeBalaMarciano = 3;
 	
 	private static float velocidadeMarciano = 5;
 	
@@ -60,7 +67,6 @@ public class main extends BasicGame {
 	private int puntuacion = 0;
 	private boolean gameOver = false;
 	
-	
 	private punto punto_pj = new punto(anchoVentana/2-anchoPj/2,altoVentana-altoPj);
 	
 	private pj pj = new pj(punto_pj,anchoPj,altoPj,anchoCanon,altoCanon,velocidadePj,cargaBala);
@@ -68,9 +74,14 @@ public class main extends BasicGame {
 	private ArrayList<marciano> arrayMarcianos= new ArrayList <marciano> ();
 	
 	private ArrayList<bala> arrayBalas= new ArrayList <bala> ();
+	private ArrayList<bala> arrayBalasMarcianos= new ArrayList <bala> ();
 	
 	private ArrayList<cadro> arrayCadros = new ArrayList <cadro> ();
 	
+	private Image spriteMarciano1;
+	private Image spriteMarciano2;
+	
+	private int numImagenMarciano = 1;
 	
 	//FUNCIÓNS
 	
@@ -144,42 +155,76 @@ public class main extends BasicGame {
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException {
 		
-		//ponemos color por defecto blanco
+		//debuxamos suelo
 		
-		g.setColor(new Color(255, 255, 255));
+		int gris = 10; 
 		
+		for (int i=altoVentana-altoVentana/4;i<altoVentana;i += 5){
+			g.setColor(new Color(gris, gris, gris));
+			g.fillRect(0,i,anchoVentana,altoVentana-i);
+			gris += 3;
+		}
+
 		//debuxamos tanque (2 rectangulos: vehiculo + canón)
 		
-		g.fillRect(pj.punto.x,pj.punto.y,pj.ancho,pj.alto);
+		if (pj.carga == cargaBala){
+			g.setColor(new Color(200, 200, 200));
+		}
+		else{
+			g.setColor(new Color(200, 200, (200+cargaBala)-(int)pj.carga));
+		}
+		
+		if (gameOver) {
+			g.setColor(new Color(255, 100, 100));
+		}
+		
+		g.fillRect(pj.punto.x+pj.ancho/8,pj.punto.y,pj.ancho-pj.ancho/4,pj.alto/2);
+		g.fillRect(pj.punto.x,pj.punto.y+pj.alto/2,pj.ancho,pj.alto/2);
 		g.fillRect(pj.punto.x+pj.ancho/2-pj.anchoCanon/2, pj.punto.y-pj.altoCanon, pj.anchoCanon, pj.altoCanon);
 		
 		//debuxamos balas
 		
 		for (int i=0; i<arrayBalas.size(); i++) {
+			Random rand = new Random ();
+			g.setColor(new Color(rand.nextInt((255) + 1), rand.nextInt((255) + 1), 255));
 			g.fillRect(arrayBalas.get(i).punto.x, arrayBalas.get(i).punto.y, arrayBalas.get(i).ancho, arrayBalas.get(i).alto);
+		}
+		
+		for (int i=0; i<arrayBalasMarcianos.size(); i++) {
+			Random rand = new Random ();
+			g.setColor(new Color(255, rand.nextInt((255) + 1), rand.nextInt((255) + 1)));
+			g.fillRect(arrayBalasMarcianos.get(i).punto.x, arrayBalasMarcianos.get(i).punto.y, arrayBalasMarcianos.get(i).ancho, arrayBalasMarcianos.get(i).alto);
 		}
 		
 		//debuxamos defensas
 		
-		g.setColor(new Color(120, 100, 80));
+		float color_restar = 0;
 		
 		for (int i=0; i<arrayCadros.size(); i++) {
+			g.setColor(new Color(150-(int)color_restar, 130-(int)color_restar, 110-(int)color_restar));
 			g.fillRect(arrayCadros.get(i).punto.x, arrayCadros.get(i).punto.y, arrayCadros.get(i).ancho, arrayCadros.get(i).alto);
+			color_restar += 0.20;
 		}
-		
-		g.setColor(new Color(255, 255, 255));
 		
 		//debuxamos marcianos
 		
 		for (int i=0; i<arrayMarcianos.size(); i++) {
 			if (!arrayMarcianos.get(i).morto) {
-				g.setColor(new Color(20,150,20));
-				g.fillRect(arrayMarcianos.get(i).punto.x,arrayMarcianos.get(i).punto.y,arrayMarcianos.get(i).ancho,arrayMarcianos.get(i).alto);
+				g.setColor(new Color(200,200,200));
+				//g.fillRect(arrayMarcianos.get(i).punto.x,arrayMarcianos.get(i).punto.y,arrayMarcianos.get(i).ancho,arrayMarcianos.get(i).alto);
+				if (numImagenMarciano == 1) {
+					spriteMarciano1.draw(arrayMarcianos.get(i).punto.x-anchoMarciano/8,arrayMarcianos.get(i).punto.y-altoMarciano/8,arrayMarcianos.get(i).ancho+anchoMarciano/4,arrayMarcianos.get(i).alto+altoMarciano/4);
+				}
+				else {
+					spriteMarciano2.draw(arrayMarcianos.get(i).punto.x-anchoMarciano/8,arrayMarcianos.get(i).punto.y-altoMarciano/8,arrayMarcianos.get(i).ancho+anchoMarciano/4,arrayMarcianos.get(i).alto+altoMarciano/4);
+				}
 				g.setColor(new Color(255,255,255));
 			}
 		}
 		
 		//debuxamos puntuacion
+		
+		g.setColor(new Color(255, 255, 255));
 		
 		g.drawString("score: "+puntuacion, 5, 0);
 		if (numMarcianosVivos <= 0) {
@@ -195,7 +240,10 @@ public class main extends BasicGame {
 
 	@Override
 	public void init(GameContainer gc) throws SlickException {
-			
+		
+		spriteMarciano1 = new Image("img/sprite_marciano1.png");
+		spriteMarciano2 = new Image("img/sprite_marciano2.png");
+	
 	}
 
 	@Override
@@ -252,7 +300,7 @@ public class main extends BasicGame {
 			}
 		}
 		
-		//movemento bala
+		//movemento balas
 		
 		for (int i=0; i<arrayBalas.size(); i++) {
 			arrayBalas.get(i).punto.y -= arrayBalas.get(i).velocidade;
@@ -261,11 +309,29 @@ public class main extends BasicGame {
 			}
 		}
 		
+		for (int i=0; i<arrayBalasMarcianos.size(); i++) {
+			arrayBalasMarcianos.get(i).punto.y += arrayBalasMarcianos.get(i).velocidade;
+			if (arrayBalasMarcianos.get(i).punto.y > altoVentana) {
+				arrayBalasMarcianos.remove(i);
+			}
+		}
+		
+		//disparo marcianos
+		
+		if (!gameOver) {
+			for (int i=0; i<arrayMarcianos.size(); i++) {
+					Random rand = new Random ();
+					if (rand.nextInt((1000)) > 998 && !arrayMarcianos.get(i).morto && arrayBalasMarcianos.size() < 4) {
+						arrayBalasMarcianos.add(new bala(new punto(arrayMarcianos.get(i).punto.x+anchoMarciano/2-anchoBalaMarciano/2,arrayMarcianos.get(i).punto.y+altoMarciano), anchoBalaMarciano, altoBalaMarciano, velocidadeBalaMarciano));
+				}
+			}
+		}
+		
 		//movemento marcianos
 		
 			//movemento horizontal
 		
-		if (yMaior > altoVentana - altoMarciano*2) {
+		if (yMaior > altoVentana - altoMarciano*4) {
 			gameOver = true;
 		}
 		
@@ -295,6 +361,13 @@ public class main extends BasicGame {
 					arrayMarcianos.get(i).punto.x = anchoVentana-anchoMarciano;
 				}
 					
+			}
+			
+			if (numImagenMarciano == 1) {
+				numImagenMarciano = 2;
+			}
+			else {
+				numImagenMarciano = 1;
 			}
 		}
 		
@@ -349,7 +422,7 @@ public class main extends BasicGame {
 					if (rectanguloBala.intersects(rectanguloMarciano)) {
 						arrayMarcianos.get(u).morto = true;
 						arrayBalas.remove(i);
-						pj.carga += cargaBala/4;
+						pj.carga += cargaBala/2;
 						numMarcianosVivos -= 1;
 						puntuacion += 100;
 						descansoMarcianos0 = descansoMarcianosInicio - (numMarcianos-numMarcianosVivos);
@@ -368,17 +441,50 @@ public class main extends BasicGame {
 		
 	for (int i=0; i<arrayBalas.size(); i++) {
 		
+		boolean choque_bala = false;
+		
 		for (int u=0; u<arrayCadros.size(); u++) {
 			Rectangle rectanguloCadro = new Rectangle(arrayCadros.get(u).punto.x, arrayCadros.get(u).punto.y, arrayCadros.get(u).ancho, arrayCadros.get(u).alto);
 			Rectangle rectanguloBala = new Rectangle(arrayBalas.get(i).punto.x, arrayBalas.get(i).punto.y, arrayBalas.get(i).ancho, arrayBalas.get(i).alto);
 			if (rectanguloBala.intersects(rectanguloCadro)) {
 				arrayCadros.remove(u);
-				arrayBalas.remove(i);
-				pj.carga += cargaBala/4;
-				break;
+				choque_bala = true;
 			}
 		}
+		
+		if (choque_bala){
+			arrayBalas.remove(i);
+			pj.carga += cargaBala/2;
+		}
 	}
+	
+	for (int i=0; i<arrayBalasMarcianos.size(); i++) {
+		
+		boolean choque_bala = false;
+		
+		for (int u=0; u<arrayCadros.size(); u++) {
+			Rectangle rectanguloCadro = new Rectangle(arrayCadros.get(u).punto.x, arrayCadros.get(u).punto.y, arrayCadros.get(u).ancho, arrayCadros.get(u).alto);
+			Rectangle rectanguloBala = new Rectangle(arrayBalasMarcianos.get(i).punto.x, arrayBalasMarcianos.get(i).punto.y, arrayBalasMarcianos.get(i).ancho, arrayBalasMarcianos.get(i).alto);
+			if (rectanguloBala.intersects(rectanguloCadro)) {
+				arrayCadros.remove(u);
+				choque_bala = true;
+			}
+		}
+		
+		if (choque_bala){
+			arrayBalasMarcianos.remove(i);
+			}
+		}
+	
+	
+	for (int i=0; i<arrayBalasMarcianos.size(); i++) {
+		Rectangle rectanguloBala = new Rectangle(arrayBalasMarcianos.get(i).punto.x, arrayBalasMarcianos.get(i).punto.y, arrayBalasMarcianos.get(i).ancho, arrayBalasMarcianos.get(i).alto);
+		Rectangle rectanguloPj = new Rectangle(pj.punto.x,pj.punto.y,pj.ancho,pj.alto);
+		if (rectanguloBala.intersects(rectanguloPj)) {
+			gameOver = true;
+			arrayBalasMarcianos.remove(i);
+			}
+		}
 	
 	}
 	
@@ -388,6 +494,7 @@ public class main extends BasicGame {
 		crearDefensas();
 		Posicions();
 		arrayBalas.clear();
+		arrayBalasMarcianos.clear();
 		descansoMarcianos0 = descansoMarcianosInicio;
 		direccionMarcianos = 0;
 		numMarcianosVivos = numMarcianos;
